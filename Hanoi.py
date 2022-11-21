234f04a11b5f3dbc47bba560a91b9558026e61d8
haohao1-12 <LiShenghao2020@outlook.com>
2022-11-21 15:28:35 +0800

    Hanoi

---
+++ Hanoi.py
@@ -0,0 +1,18 @@
+'''
+首先将上层N-1个盘片的盘片塔，从开始柱，经由目标柱，移动到中间柱；
+然后将第N个（最大的）盘片，从开始柱，移动到目标柱；
+最后将放置在中间柱的N-1个盘片的盘片塔，经由开始柱，移动到目标柱
+
+基本结束条件，也就是最小规模问题是：1个盘片的移动问题
+'''
+
+def moveTower(height, fromPole, withPole, toPole):
+    if height >= 1:
+        moveTower(height -1, fromPole, toPole, withPole)
+        moveDisk(height, fromPole, toPole)
+        moveTower(height - 1, withPole, fromPole, toPole)
+
+def moveDisk(disk, fromPole, toPole):
+    print(f"Moving disk[{disk}] from {fromPole} to {toPole}")
+
+moveTower(3, "#1","#2","#3")
