5687f354066a54af00d8cfb832e52e6a933e4df8
haohao1-12 <LiShenghao2020@outlook.com>
2022-11-25 09:54:33 +0800

    update

--- BinarySearch.py
+++ BinarySearch.py
@@ -13,6 +13,8 @@ def binarySearch(alist, item):
             else:
                 first = midpoint + 1
 
+    return found
+
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
 print(binarySearch(testlist, 3))
 print(binarySearch(testlist, 13))
