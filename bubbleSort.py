67abff5937e8b14a8cac272fb314ad24a381425d
haohao1-12 <LiShenghao2020@outlook.com>
2022-11-25 16:11:58 +0800

    update

--- bubbleSort.py
+++ bubbleSort.py
@@ -7,3 +7,18 @@ def bubbleSort(alist):
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 bubbleSort(alist)
 print(alist)
+
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist) - 1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                exchanges = True
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+        passnum = passnum - 1
+
+alist = [20, 30, 40, 90, 50, 60, 70 ,80, 100, 110]
+shortBubbleSort(alist)
+print(alist)
